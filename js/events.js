adebca08388bde9b655d6cbff00d58d07dc6fdf2
Griffin Sandler <griffinsandler@gmail.com>
2018-06-25 18:47:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,10 @@ function pressIt() {
 }
 
 function submitIt() {
-  
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+    return;
+  });
 }
 
 $(document).ready(function(){
